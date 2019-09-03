79fad767befe31bf03c980bdf0d7e284348c09a4
jensenjamison <jensenjamison@yahoo.com>
2019-09-03 13:00:54 -0500

    first commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import Home from "./Components/Home"
+import TopicList from "./Components/TopicList"
+
+
+export  default (
+    <Switch>
+        <div>
+            <Route  />
+            <Route />
+        </div>
+    </Switch>
+)
+
